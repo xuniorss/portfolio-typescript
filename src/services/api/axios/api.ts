e7882d47da35ce2183e606cb83e6d92596af6ef5
xuniorss <fortunatojunior2020@gmail.com>
2023-01-28 00:07:44 -0300

    headers - cors

--- src/services/api/axios/api.ts
+++ src/services/api/axios/api.ts
@@ -4,6 +4,7 @@ export function setupAPIClient() {
    const api = axios.create({
       // baseURL: 'http://localhost:3333',
       baseURL: 'https://portfolio-api-amber.vercel.app/',
+      headers: { 'Access-Control-Allow-Origin': '*' },
    })
 
    return api
